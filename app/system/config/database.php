0cac158ae3b4399fb344e144904d815564491b09
dancesmile <1215850394@qq.com>
2017-10-04 21:31:25 +0800

    add  wecenter

---
+++ app/system/config/database.php
@@ -0,0 +1,15 @@
+<?php
+
+$config['charset'] = 'utf8';
+$config['prefix'] = '';
+$config['driver'] = 'MySQLi';
+$config['master'] = array (
+  'charset' => 'utf8',
+  'host' => 'db',
+  'username' => 'root',
+  'password' => 'root',
+  'dbname' => 'app',
+  'port' => '3306',
+);
+$config['slave'] = false;
+$config['port'] = '3306';
