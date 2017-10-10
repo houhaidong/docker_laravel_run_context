f5f39fa203f56711e270d5cee02220b365006883
dancesmile <1215850394@qq.com>
2017-10-11 00:06:13 +0800

    add  wecenter

--- app/tmp/plugins_model.php
+++
@@ -1,1 +0,0 @@
-a:2:{s:18:"aws_external_class";s:46:"/mnt/app/plugins/aws_external/aws_external.php";s:26:"aws_offical_external_class";s:62:"/mnt/app/plugins/aws_offical_external/aws_offical_external.php";}
